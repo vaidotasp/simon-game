b5911065489a7aedf7c61bb8ac7002f6df25c988
Vaidotas <vp@Mac.local>
2017-07-07 06:10:18 -0400

    reset function implementation

--- app.js
+++ app.js
@@ -10,42 +10,95 @@ document.addEventListener("DOMContentLoaded", function() {
   const strictBtn = document.getElementById('strict-btn');
   strictBtn.addEventListener('click', strict)
 
+  const first = document.getElementById('1');
+  first.addEventListener('click', one)
+
+  const second = document.getElementById('2');
+  second.addEventListener('click', two)
+
+  const third = document.getElementById('3');
+  third.addEventListener('click', three)
+
+  const fourth = document.getElementById('4');
+  fourth.addEventListener('click', four)
+
+  function one() {
+    console.log("Clicked");
+    let audio = document.getElementById('one');
+    audio.play();
+  }
+
+  function two() {
+    console.log("Clicked");
+    let audio = document.getElementById('two');
+    audio.play();
+  }
+
+  function three() {
+    console.log("Clicked");
+    let audio = document.getElementById('three');
+    audio.play();
+  }
+
+  function four() {
+    console.log("Clicked");
+    let audio = document.getElementById('four');
+    audio.play();
+  }
 
   function power() {
     console.log("Power button clicked");
+    //power flip functionality
+    let switcher = document.getElementById("power-btn");
+    if (switcher.children[0].className === "switch-on") {
+      switcher.children[0].className = "switch";
+    } else {
+      switcher.children[0].className = "switch-on";
+    }
+    //reset the game to original state
+    if (originalState.count !== 0) {
+      originalState.count = 0;
+      originalState.sequence = [];
+      originalState.strictMode = false;
+      let countDisplay = document.getElementsByClassName("c-display");
+      countDisplay[0].innerHTML = originalState.count;
+    }
   }
 
   function start() {
     console.log("Start button clicked");
+    originalState.randomFill();
+    console.log(originalState)
+  }
+
+  function playSequence() {
+
   }
 
   function strict() {
     console.log("Strict button clicked");
   }
 
-  const first = document.getElementById('1');
-  first.addEventListener('click', one)
-  const second = document.getElementById('2');
-  second.addEventListener('click', two)
-  const third = document.getElementById('3');
-  third.addEventListener('click', three)
-  const fourth = document.getElementById('4');
-  fourth.addEventListener('click', four)
-
-  function one() {
-    console.log("Clicked");
+  //Handler stuff done
+  //State setup:
+  let originalState = {
+      count: 10,
+      sequence: [],
+      strictMode: false,
+      randomFill: function() {
+        for (let i = 0; i < 20; i++) {
+          this.sequence.push(randomInt());
+        }
+      }
+    }
+    //randomizer between 1 and 4
+  function randomInt() {
+    let min = Math.ceil(1);
+    let max = Math.floor(4);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function two() {
-    console.log("Clicked");
-  }
 
-  function three() {
-    console.log("Clicked");
-  }
 
-  function four() {
-    console.log("Clicked");
-  }
 
 });
